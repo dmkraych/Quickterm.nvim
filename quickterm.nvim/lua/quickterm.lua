4cdc4f5486cf0d2b807e11899e300e48a6039e1e
Derik <dmkraych@gmail.com>
2025-04-04 22:23:14 -0600

    Initial commit

---
+++ quickterm.nvim/lua/quickterm.lua
@@ -0,0 +1,54 @@
+local quickterm = {}
+
+quickterm.state = {
+  quick = {
+    buf = -1,
+    win = -1,
+  },
+}
+
+function quickterm.setup(opts) end
+
+function quickterm.create_quick_win(opts)
+  opts = opts or {}
+  local width = vim.o.columns
+  local height = 10
+  local col = vim.o.columns - 1
+  local row = vim.o.lines - 1
+
+  local buf = nil
+  if vim.api.nvim_buf_is_valid(opts.buf) then
+    buf = opts.buf
+  else
+    buf = vim.api.nvim_create_buf(false, true)
+  end
+
+  local win_config = {
+    relative = "editor",
+    width = width,
+    height = height,
+    col = col,
+    row = row,
+    border = "rounded",
+  }
+
+  local win = vim.api.nvim_open_win(buf, true, win_config)
+
+  return { buf = buf, win = win }
+end
+
+function quickterm.toggle_terminal()
+  if not vim.api.nvim_win_is_valid(quickterm.state.quick.win) then
+    quickterm.state.quick = quickterm.create_quick_win({ buf = quickterm.state.quick.buf })
+    if vim.bo[quickterm.state.quick.buf].buftype ~= "terminal" then
+      vim.cmd.terminal()
+    end
+  else
+    vim.api.nvim_win_hide(quickterm.state.quick.win)
+  end
+end
+
+vim.api.nvim_create_user_command("Quickterm", quickterm.toggle_terminal, {})
+vim.keymap.set("n", "<leader>st", quickterm.toggle_terminal)
+
+return quickterm
